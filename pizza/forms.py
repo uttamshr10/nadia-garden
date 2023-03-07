eaa34abc099e2f426a505d85273b81e531aea667
Uttam <utam.shrestha65@gmail.com>
2023-03-07 17:26:44 +0545

    model forms

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from pizza import models
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(max_length=100)
+#     topping2 = forms.CharField(max_length=100)
+#     size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'),('Medium','Medium'),('Large','Large')])
 
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(max_length=100)
-    topping2 = forms.CharField(max_length=100)
-    size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'),('Medium','Medium'),('Large','Large')])
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = models.Pizza
+        fields = '__all__'
+        labels = {
+            'topping1' : 'Topping 1',
+            'topping2' : 'Topping 2'
+        }
