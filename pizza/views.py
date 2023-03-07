ffb0521fb253b05ec1fe1000fb41beefd4b985df
Uttam <utam.shrestha65@gmail.com>
2023-03-07 16:40:58 +0545

    submitted data handler

--- pizza/views.py
+++ pizza/views.py
@@ -6,5 +6,21 @@ def home(request):
     return render(request, 'pizza/home.html')
 
 def order(request):
-    form = PizzaForm()
-    return render(request, 'pizza/order.html', {'pizzaform': form})
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Thanks for ordering! Your %s %s and %s pizza is on its way!' %(filled_form.cleaned_data['size'],
+              filled_form.cleaned_data['topping1'],
+              filled_form.cleaned_data['topping2'])
+            new_form = PizzaForm()
+            context = {
+                'pizzaform': new_form,
+                'note': note
+            }
+            return render(request, 'pizza\order.html', context)
+    else:
+        form = PizzaForm()
+        context = {
+            'pizzaform': form
+        }
+        return render(request, 'pizza/order.html', context)
