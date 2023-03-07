4636d72a453572682ea2418d5d3dfc586c5975a0
Uttam <utam.shrestha65@gmail.com>
2023-03-07 19:13:33 +0545

    Saving the user input to DB

--- pizza/views.py
+++ pizza/views.py
@@ -10,6 +10,7 @@ def order(request):
     if request.method == 'POST':
         filled_form = forms.PizzaForm(request.POST)
         if filled_form.is_valid():
+            filled_form.save()
             note = 'Thanks for ordering! Your %s %s and %s pizza is on its way!' %(filled_form.cleaned_data['size'],
               filled_form.cleaned_data['topping1'],
               filled_form.cleaned_data['topping2'])
