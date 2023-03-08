add88692989effe8f575b26db2bdf00aa21ad316
Uttam <utam.shrestha65@gmail.com>
2023-03-08 08:06:58 +0545

    Secret Key hidden

--- djangoforms/settings.py
+++ djangoforms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&^efm(pc(1fxb1s3kmnpvs0r__dvo5py8fvfie=y$4w5)0scto'
+SECRET_KEY = config('SECRET_KEY', default = '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast = bool, default = True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in config('ALLOWED_HOSTS', default = '').split(',')
+    ]
 
 
 # Application definition
