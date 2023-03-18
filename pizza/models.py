d7d19b2f0fa1cf976f4ac7bb8c62ff97745ac2fe
Uttam <utam.shrestha65@gmail.com>
2023-03-18 12:07:29 +0545

    stringified

--- pizza/models.py
+++ pizza/models.py
@@ -11,3 +11,5 @@ class Pizza(models.Model):
     topping1 = models.CharField(max_length=100)
     topping2 = models.CharField(max_length=100)
     size = models.ForeignKey('Sizes', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.topping1
